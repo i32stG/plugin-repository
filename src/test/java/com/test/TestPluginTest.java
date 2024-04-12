839881c8a37850ad5f9ef64bb86781902cd212e5
George <george_b_93@hotmail.co.uk>
2024-04-12 12:35:21 +0100

    initial config

--- src/test/java/com/test/TestPluginTest.java
+++ src/test/java/com/test/TestPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.test;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class TestPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(TestPlugin.class);
 		RuneLite.main(args);
 	}
 }
